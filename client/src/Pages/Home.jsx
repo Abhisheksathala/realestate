9a85ff200b9c7f6441597aea762cd43429d82650
Abhishek-Sathala <abhisheksathala296@gmail.com>
2024-10-16 13:31:22 +0530

    home

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import SwiperCore from "swiper";
 import "swiper/css/bundle";
-import Listingitem from "../components/Listingitem.jsx";
+import Listingitem from "../components/Listingitem";
 
 export default function Home() {
   const [offerListings, setOfferListings] = useState([]);
